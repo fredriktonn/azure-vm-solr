a731b4222f6911bbf8e93334db865324c13dd561
Fredrik Tonn <frtonn@microsoft.com>
2018-06-13 13:59:13 +0200

    simplified install script

--- InstallSolR.ps1
+++ InstallSolR.ps1
@@ -3,102 +3,19 @@ cls
 New-Item "c:\install-log" -type Directory -force | Out-Null
 $LogFile = "c:\install-log\install.log"
 
-# Get username/password & machine name
-$userName = "artifactInstaller"
-[Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
-$password = $([System.Web.Security.Membership]::GeneratePassword(12,4))
-$cn = [ADSI]"WinNT://$env:ComputerName"
-
-# Create new user
-$user = $cn.Create("User", $userName)
-$user.SetPassword($password)
-$user.SetInfo()
-$user.description = "Choco artifact installer"
-$user.SetInfo()
-
-# Add user to the Administrators group
-$group = [ADSI]"WinNT://$env:ComputerName/Administrators,group"
-$group.add("WinNT://$env:ComputerName/$userName")
-
-# Create pwd and new $creds for remoting
-$secPassword = ConvertTo-SecureString $password -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
-
-# Ensure that current process can run scripts. 
-"Enabling remoting" | Out-File $LogFile -Append
-Enable-PSRemoting -Force -SkipNetworkProfileCheck
-
 "Changing ExecutionPolicy" | Out-File $LogFile -Append
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
 
 # Install Choco
 "Installing Chocolatey" | Out-File $LogFile -Append
-$sb = { iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) }
-Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
+Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 "Disabling UAC" | Out-File $LogFile -Append
-$sb = { Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0 }
-Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential
+Set-ItemProperty -path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -name EnableLua -value 0
 
 #"Install each Chocolatey Package"
-$command = "cd C:\ProgramData\chocolatey\bin"
-$command | Out-File $LogFile -Append
-$sb = [scriptblock]::Create("$command")
+Set-Location C:\ProgramData\chocolatey\bin | Out-File $LogFile -Append
+choco install solr --version 6.6.2 -y -force | Out-File $LogFile -Append
 
-# Use the current user profile
-try
-{
-    Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
-}
-catch
-{
-    $_.Exception | format-list -force | Out-File $LogFile -Append
-}
-
-$command = "choco install solr --version 6.6.2 -y -force"
-$command | Out-File $LogFile -Append
-$sb = [scriptblock]::Create("$command")
-
-# Use the current user profile
-try
-{
-    Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
-}
-catch
-{
-    $_.Exception | format-list -force | Out-File $LogFile -Append
-}
-
-$command = "cd C:\tools\solr-6.6.2\bin"
-$command | Out-File $LogFile -Append
-$sb = [scriptblock]::Create("$command")
-
-try
-{
-    Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
-}
-catch
-{
-    $_.Exception | format-list -force | Out-File $LogFile -Append
-}
-
-$command = "solr start"
-$command | Out-File $LogFile -Append
-$sb = [scriptblock]::Create("$command")
-
-try
-{
-    Invoke-Command -ScriptBlock $sb -ComputerName $env:COMPUTERNAME -Credential $credential | Out-Null
-}
-catch
-{
-    $_.Exception | format-list -force | Out-File $LogFile -Append
-}
-
-Disable-PSRemoting -Force
-
-# Delete the artifactInstaller user
-$cn.Delete("User", $userName)
-
-# Delete the artifactInstaller user profile
-gwmi win32_userprofile | where { $_.LocalPath -like "*$userName*" } | foreach { $_.Delete() }
+Set-Location C:\tools\solr-6.6.2\bin | Out-File $LogFile -Append
+solr start | Out-File $LogFile -Append
